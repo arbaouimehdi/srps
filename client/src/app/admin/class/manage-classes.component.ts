f0de37d4f86ad97fc6b9f11cda06c16d1cd26052
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-04-16 23:19:25 +0100

    Add Data Table

--- client/src/app/admin/class/manage-classes.component.ts
+++ client/src/app/admin/class/manage-classes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
 
 @Component({
   selector: 'app-manage-classes',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageClassesComponent implements OnInit {
 
+  displayedColumns = ['position', 'className', 'classNameNumeric', 'section', '_id'];
+  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
   }
 
 }
+
+export interface Element {
+  className: string;
+  classNameNumeric: number;
+  section: string;
+  position: number;
+  _id: number;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, className: 'One', classNameNumeric: 4, section: 'A', _id: 24},
+  {position: 1, className: 'One', classNameNumeric: 4, section: 'A', _id: 24},
+  {position: 1, className: 'One', classNameNumeric: 4, section: 'A', _id: 24},
+  {position: 1, className: 'One', classNameNumeric: 4, section: 'A', _id: 24},
+];
