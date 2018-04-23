c6edbf6b58bcf768d708f0100f41aa6d098ce9e6
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-04-23 20:17:11 +0100

    Add & Manage Sbjects

--- client/src/app/admin/subject/manage-subjects.component.ts
+++ client/src/app/admin/subject/manage-subjects.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
 
 @Component({
   selector: 'app-manage-subjects',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageSubjectsComponent implements OnInit {
 
+  displayedColumns = [
+    'position',
+    'subjectName',
+    'subjectCode',
+    'creationDate',
+    '_id'
+  ];
+
+  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
 }
+
+export interface Element {
+  position: number;
+  subjectName: string;
+  subjectCode: number;
+  creationDate: string;
+  _id: number;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, subjectName: 'English', subjectCode: 6578, creationDate:'2018-02-25 12:11:12', _id: 1},
+  {position: 2, subjectName: 'Math', subjectCode: 128, creationDate:'2018-02-25 12:11:12', _id: 2},
+];
