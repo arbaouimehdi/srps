263834380efb108fd495f9b49650bc6e26a3f080
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-04-23 20:02:35 +0100

    Add Manage Students

--- client/src/app/admin/student/manage-student.component.ts
+++ client/src/app/admin/student/manage-student.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatTableDataSource, MatSort, MatPaginator} from '@angular/material';
 
 @Component({
   selector: 'app-manage-student',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageStudentComponent implements OnInit {
 
+  displayedColumns = [
+    'position',
+    'studentName',
+    'rollId',
+    'classId',
+    'registrationDate',
+    'status',
+    '_id'
+  ];
+
+  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+
 }
+
+export interface Element {
+  position: number;
+  studentName: string;
+  rollId: number;
+  classId: number;
+  registrationDate: string;
+  status: number;
+  _id: number;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, studentName: 'Ashish Shrivastav', rollId: 2147, classId: 75, registrationDate:'2018-02-25 12:11:12', status: 1, _id: 12}
+];
