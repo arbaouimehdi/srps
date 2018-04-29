ce05a20abd1ba6adc5ce76ee6139ad87e256a5e6
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-04-29 20:50:19 +0100

    Add a Subject

--- client/src/app/admin/subject/add-subject.component.ts
+++ client/src/app/admin/subject/add-subject.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+// Models
+import { Subject } from '../../shared/models/subject.model';
+
+// Services
+import { SubjectsService } from '../../shared/services/subject.service';
+
+// Resolvers
+import { SubjectResolver } from './subjects-resolver.service';
 
 @Component({
   selector: 'app-add-subject',
@@ -7,9 +18,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddSubjectComponent implements OnInit {
 
-  constructor() { }
+  isSubmitting = false;
+  subject: Subject = {} as Subject;
+  subjectForm: FormGroup;
 
+  /**
+   * Constructor
+   *
+   *
+   */
+  constructor(
+    private subjectsService: SubjectsService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private router: Router,
+  ) {
+    this.subjectForm = this.fb.group({
+      name: '',
+      code: ''
+    });
+  }
+
+  /**
+   *
+   * Init
+   *
+   */
   ngOnInit() {
+  }
+
+  /**
+   *
+   * Add an Item
+   *
+   */
+  addItem(){
+    this.isSubmitting = true;
+
+    this.subjectsService
+      .add(this.subjectForm.value)
+      .subscribe(
+        success => {
+          this.router.navigateByUrl('admin/manage-subjects')
+        },
+        err => {
+          this.isSubmitting = false;
+        }
+    );
   }
 
 }
