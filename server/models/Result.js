271b173576a3f46182a3affbae5183e03a504e6b
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-05-07 21:05:24 +0100

    Make Student and Class Combination Unique

--- server/models/Result.js
+++ server/models/Result.js
@@ -2,11 +2,30 @@ let mongoose = require('mongoose');
 let uniqueValidator = require('mongoose-unique-validator');
 
 let ResultSchema = new mongoose.Schema({
-  student: {type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
-  classe: {type: mongoose.Schema.Types.ObjectId, ref: 'Classe', required: true },
-  subject: {type: mongoose.Schema.Types.ObjectId, ref: 'Subject', required: true },
-  score: { type: Number, required: true },
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Student',
+    required: true
+  },
+  classe: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Classe',
+    required: true,
+  },
+  subject: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subject',
+    required: true 
+  },
+  score: { 
+    type: Number,
+    required: true 
+  },
 }, {timestamps: true});
+
+ResultSchema.index({ student: 1, classe: 1 }, { unique: true });
+
+ResultSchema.plugin(uniqueValidator);
 
 ResultSchema.methods.toJSONFor = function() {
   return {
